e6b43b917859fdad71f66aebcf0ed4021f26b7ab
Rene Pollesch <rene.pollesch@gmx.de>
2014-10-18 17:52:48 +0200

    added setPen(), setPaper(), setInk() to CPC API

--- lib/cpc.s
+++ lib/cpc.s
@@ -10,7 +10,7 @@ _CPC_SetMode:
     ld      hl, #2
     add     hl, sp
     ld      a, (hl)
-    call    0xBC0E
+    jp      0xBC0E
 
 ; -----------------------------------------------------------------------------
 ; void CPC_SetBorder(char c)
@@ -23,4 +23,43 @@ _CPC_SetBorder:
     add     hl, sp
     ld      b, (hl)
     ld      c, b
-    call    0xBC38
+    jp      0xBC38
+
+; -----------------------------------------------------------------------------
+; void CPC_SetInk(unsigned char p, unsigned char c)
+; -----------------------------------------------------------------------------
+
+.globl _CPC_SetInk
+
+_CPC_SetInk:
+    ld      hl, #2
+    add     hl, sp
+    ld      a, (hl)
+    inc     hl
+    ld      b, (hl)
+    ld      c, b
+    jp      0xBC32
+
+; -----------------------------------------------------------------------------
+; void CPC_SetPen(unsigned char p)
+; -----------------------------------------------------------------------------
+
+.globl _CPC_SetPen
+
+_CPC_SetPen:
+    ld      hl, #2
+    add     hl, sp
+    ld      a, (hl)
+    jp      0xBB90
+
+; -----------------------------------------------------------------------------
+; void CPC_Paper(unsigned char p)
+; -----------------------------------------------------------------------------
+
+.globl _CPC_SetPaper
+
+_CPC_SetPaper:
+    ld      hl, #2
+    add     hl, sp
+    ld      a, (hl)
+    jp      0xBB96
--- src/main.c
+++ src/main.c
@@ -4,7 +4,11 @@
 void main()
 {
     CPC_SetMode(2);
-    CPC_SetBorder(0);
+    CPC_SetBorder(CPC_COLOR_BLACK);
+    CPC_SetInk(0, CPC_COLOR_BLACK);
+    CPC_SetInk(1, CPC_COLOR_BRIGHT_WHITE);
+    CPC_SetPen(0);
+    CPC_SetPaper(1);
 
     cputs("HELLO WORLD");
     getch();
