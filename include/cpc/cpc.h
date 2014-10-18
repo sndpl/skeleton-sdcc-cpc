0b4d51385d7088c4fdfae0e4c36cd83ec9d9ed18
Rene Pollesch <rene.pollesch@gmx.de>
2014-10-18 20:24:36 +0200

    separate constant definitions into cpc_defs.h

--- include/cpc/cpc.h
+++ include/cpc/cpc.h
@@ -1,33 +1,7 @@
+#include <cpc/cpc_defs.h>
+
 #ifndef __cpc_h__
 #define __cpc_h__
-
-#define CPC_COLOR_BLACK          0
-#define CPC_COLOR_BLUE           1
-#define CPC_COLOR_BRIGHT_BLUE    2
-#define CPC_COLOR_RED            3
-#define CPC_COLOR_MAGENTA        4
-#define CPC_COLOR_MAUVE          5
-#define CPC_COLOR_BRIGHT_RED     6
-#define CPC_COLOR_PURPLE         7
-#define CPC_COLOR_BRIGHT_MAGENTA 8
-#define CPC_COLOR_GREEN          9
-#define CPC_COLOR_CYAN           10
-#define CPC_COLOR_SKY_BLUE       11
-#define CPC_COLOR_YELLOW         12
-#define CPC_COLOR_WHITE          13
-#define CPC_COLOR_PASTEL_BLUE    14
-#define CPC_COLOR_ORANGE         15
-#define CPC_COLOR_PINK           16
-#define CPC_COLOR_PASTEL_MAGENTA 17
-#define CPC_COLOR_BRIGHT_GREEN   18
-#define CPC_COLOR_SEA_GREEN      19
-#define CPC_COLOR_BRIGHT_CYAN    20
-#define CPC_COLOR_LIME           21
-#define CPC_COLOR_PASTEL_GREEN   22
-#define CPC_COLOR_PASTEL_CYAN    23
-#define CPC_COLOR_BRIGHT_YELLOW  24
-#define CPC_COLOR_PASTEL_YELLOW  25
-#define CPC_COLOR_BRIGHT_WHITE   26
 
 /**
  * Set screen mode 0 .. 2
