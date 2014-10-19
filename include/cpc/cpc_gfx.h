02b1aef95276620b544ad68c2316149da83ba375
Rene Pollesch <rene.pollesch@gmx.de>
2014-10-19 11:53:24 +0200

    added CPC_GFX_VSync()

--- include/cpc/cpc_gfx.h
+++ include/cpc/cpc_gfx.h
@@ -11,4 +11,9 @@ void CPC_GFX_Move(int x, int y);
  */
 void CPC_GFX_Draw(int x, int y);
 
+/**
+ * Wait for v-sync
+ */
+void CPC_GFX_VSync(void);
+
 #endif
--- lib/cpc/cpc_gfx.s
+++ lib/cpc/cpc_gfx.s
@@ -63,3 +63,17 @@ _CPC_GFX_Draw:
     ld      l, c
     call    0xBBF6
     ret
+
+; -----------------------------------------------------------------------------
+; void CPC_GFX_VSync(void)
+; -----------------------------------------------------------------------------
+
+.globl _CPC_GFX_VSync
+
+_CPC_GFX_VSync:
+    ld      b, #0xF5
+
+syncwait$:
+    in      a, (c)
+    rra
+    jp      nc, syncwait$
