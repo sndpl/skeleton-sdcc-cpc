9704666412e4d03a1def4dba338bad30435358e4
poliander <r.pollesch@gmx.net>
2015-01-01 14:43:49 +0100

    added cpc_fw_km[.h] library

---
+++ include/cpc/cpc_fw_km.h
@@ -0,0 +1,11 @@
+#ifndef __cpc_cpc_fw_km_h__
+#define __cpc_cpc_fw_km_h__
+
+#include "cpc.h"
+
+/**
+ * Waits for a key to be pressed
+ */
+unsigned char CPC_FW_WaitKey(void);
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,21 +1,38 @@
 #include <conio.h>
 
 #include <cpc/cpc_fw_gra.h>
+#include <cpc/cpc_fw_km.h>
 #include <cpc/cpc_fw_scr.h>
 #include <cpc/cpc_fw_txt.h>
 
 void main()
 {
+    unsigned char c = 0;
+    unsigned char done = 0;
+
     CPC_FW_SetMode(2);
     CPC_FW_SetBorder(CPC_COLOR_BLACK);
     CPC_FW_SetInk(0, CPC_COLOR_BLACK);
     CPC_FW_SetInk(1, CPC_COLOR_BRIGHT_WHITE);
-    CPC_FW_SetPen(0);
-    CPC_FW_SetPaper(1);
+    CPC_FW_SetPen(1);
+    CPC_FW_SetPaper(0);
 
     CPC_FW_Move(0, 381);
     CPC_FW_Draw(87, 381);
 
-    cputs("HELLO WORLD");
-    getch();
+    cputs("HELLO WORLD > ");
+
+    while(done == 0)
+    {
+        c = CPC_FW_WaitKey();
+
+        if (c == 13)
+        {
+            done = 1;
+        }
+        else
+        {
+            putch(c);
+        }
+    }
 }
